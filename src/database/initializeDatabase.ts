0e7a1b59841f4d65acabe89f22df718b48bb842f
Eduardo Ribeiro da Silva <eduardo20003131@gmail.com>
2024-08-02 02:15:14 -0300

    Remove Alert and ajust Try Catch block

--- src/database/initializeDatabase.ts
+++ src/database/initializeDatabase.ts
@@ -1,20 +1,25 @@
-import { type SQLiteDatabase } from 'expo-sqlite'
+import { SQLiteDatabase } from 'expo-sqlite';
 
 export async function initializeDatabase(database: SQLiteDatabase) {
-    await database.execAsync(`
-      CREATE TABLE IF NOT EXISTS property (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome_imovel TEXT NOT NULL,
-        descricao TEXT,
-        cep TEXT,
-        endereco TEXT,
-        numero INTEGER,
-        complemento TEXT,
-        cidade TEXT,
-        uf TEXT,
-        preco REAL,
-        imagem TEXT,
-        disponivel BOOLEAN
-      );
-    `)
-} 
+    try {
+        await database.execAsync(`
+          CREATE TABLE IF NOT EXISTS property (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome_imovel TEXT NOT NULL,
+            descricao TEXT,
+            cep TEXT,
+            endereco TEXT,
+            numero INTEGER,
+            complemento TEXT,
+            cidade TEXT,
+            uf TEXT,
+            preco REAL,
+            imagem TEXT,
+            disponivel BOOLEAN
+          );
+        `);
+    } catch (error) {
+        console.error("Falha ao inicializar o banco de dados:", error);
+        throw new Error("Falha ao inicializar o banco de dados");
+    }
+}
