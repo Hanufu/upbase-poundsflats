2ef3e5199e9b1088f9e7373054acb48a37156f7b
Eduardo Ribeiro da Silva <eduardo20003131@gmail.com>
2024-08-01 23:40:20 -0300

    fix: update image import path in PropertyItem component
    - Changed image import method to use `require()` with relative path.
    - Resolved import issues for PropertyImage.png by using dynamic import approach.

---
+++ src/components/EmptyPropertyList/PropertyItem/PropertyItem.tsx
@@ -0,0 +1,62 @@
+import { View, Text, Image, StyleSheet } from 'react-native';
+import React from 'react';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+
+export default function PropertyItem() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={require('@/assets/images/PropertyImage.png')} />
+      <View style={styles.section1}>
+        <Text style={[styles.text, styles.nome]}>Poundsflats Aquário</Text>
+        <AntDesign name="star" size={16} color="#7B2CBF" />
+        <Text style={[styles.text, styles.nota]}>4.5</Text>
+      </View>
+      <View style={styles.section2}>
+        <Text style={[styles.text, styles.local]}>Localização</Text>
+        <Text style={[styles.text, styles.valor]}>Diária: R$97</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 16,
+  },
+  image: {
+    width: 345,
+    height: 250,
+    borderRadius: 24,
+  },
+  section1: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 20,
+  },
+  section2: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 16,
+  },
+  text: {
+    fontFamily: 'Jura_400Regular',
+    color: '#10002B',
+    lineHeight: 20,
+    fontSize: 16,
+  },
+  nome: {
+    fontFamily: 'Jura_500Medium',
+    letterSpacing: -0.17,
+  },
+  nota: {},
+  local: {
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  valor: {
+    fontFamily: 'Jura_500Medium',
+  },
+});
